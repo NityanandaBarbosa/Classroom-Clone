193017e96733c3c805d9b1c6b8dc57acb05b1e35
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2022-07-25 15:41:32 -0300

    feat : first commit

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+
+import 'home_store.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  final HomeStore store;
+  const HomePage({
+    Key? key,
+    this.title = "Teste",
+    required this.store,
+  }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: ScopedBuilder<HomeStore, Exception, int>(
+        store: widget.store,
+        onState: (_, counter) {
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text('$counter'),
+          );
+        },
+        onError: (context, error) => const Center(
+          child: Text(
+            'Too many clicks',
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          widget.store.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
