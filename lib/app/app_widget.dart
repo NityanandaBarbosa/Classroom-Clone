17576745eb1c45a60dcec445d67aa972f550601b
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2022-08-02 16:06:27 -0300

    feat : redirect to login when token is expired or invalid pt2

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+import 'package:ifroom/app/core/errors/token_expired_or_invalid.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  AppWidget({Key? key}) : super(key: key);
+
+  final GlobalKey<ScaffoldMessengerState> snackbarKey =
+      GlobalKey<ScaffoldMessengerState>();
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
+    TripleObserver.addListener((triple) {
+      if (triple.error is TokenExpiredOrInvalid) {
+        const snackBar = SnackBar(
+          content: Text("Session expired, redirecting to login!"),
+          backgroundColor: Colors.red,
+        );
+        snackbarKey.currentState?.showSnackBar(snackBar);
+      }
+    });
     return MaterialApp.router(
       title: 'Flutter Slidy',
-      theme: ThemeData(primarySwatch: Colors.green), 
-      routeInformationParser: Modular.routeInformationParser, 
+      scaffoldMessengerKey: snackbarKey,
+      theme: ThemeData(primarySwatch: Colors.green),
+      routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
     );
   }
